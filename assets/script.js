c1e7486a1f82029e93006a7afcc7c2e06e93fbfb
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2022-12-23 14:30:35 -0800

    added more conent, navbar.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+console.log("This is just a test!")
+console.log(100);
+// remember varible is like a constainer, the number is like a value
+var myRandomValue = 100;
+myRandomValue = myRandomValue *2
+console.log(myRandomValue)
+
+document
