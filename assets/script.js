aa6f3ea877510d0f1f4f4729dc5ba6219d1e10bb
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-01-01 16:04:07 -0800

    added resume

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,7 @@
 console.log("This is just a test!")
 console.log(100);
-// remember varible is like a constainer, the number is like a value
+// remember varible is like a constainer, the number is a value
 var myRandomValue = 100;
 myRandomValue = myRandomValue *2
 console.log(myRandomValue)
 
-document
